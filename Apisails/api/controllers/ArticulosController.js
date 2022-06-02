d289d0e75da3492c75b73e1443c981b1b9b6a1a7
Gilbertoyan <gilbertoyan7@gmail.com>
2022-06-02 11:34:13 -0600

    funcion para buscar mis usuarios y articulos

--- Apisails/api/controllers/ArticulosController.js
+++ Apisails/api/controllers/ArticulosController.js
@@ -5,8 +5,13 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const Articulos = require("../models/Articulos");
+
 module.exports = {
-  
+    getAllUsers: async function (req, res){
+        var misArticulos = await Articulos.find();
+        res.ok(misArticulos)
+      }
 
 };
 
